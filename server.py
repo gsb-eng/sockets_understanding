032908664d063ddeb9251637c573e899915402fb
Srinivas Garlapati <srinivasa.b.garlapati@gmail.com>
2015-04-09 22:54:00 +0530

    Modifing socket programme

--- server.py
+++ server.py
@@ -1,25 +1,58 @@
+#!/usr/bin/env python
+
 import socket
+import select
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+class ChatServer:
 
-BUFFER_SIZE = 20
+    def __init__( self, port ):
+        self.port = port;
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.srvsock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+        self.srvsock.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
+        self.srvsock.bind( ("", port) )
+        self.srvsock.listen( 5 )
 
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+        self.descriptors = [self.srvsock]
+        print 'Server started on port %s' % port
 
-conn, addr = s.accept()
+    def run( self ):
+        while 1:
+            # Await an event on a readable socket descriptor
+            (sread, swrite, sexc) = select.select( self.descriptors, [], [] )
+            # Iterate through the tagged read descriptors
+            for sock in sread:
+                # Received a connect to the server (listening) socket
+                if sock == self.srvsock:
+                    self.accept_new_connection()
+                else:
+                    # Received something on a client socket
+                    str = sock.recv(100)
 
-print 'Connection address:', addr
+                    # Check to see if the peer socket closed
+                    if str == '':
+                        host,port = sock.getpeername()
+                        str = 'Client left %s:%s\r\n' % (host, port)
+                        self.broadcast_string( str, sock )
+                        sock.close
+                        self.descriptors.remove(sock)
+                    else:
+                        host,port = sock.getpeername()
+                        newstr = '[%s:%s] %s' % (host, port, str)
+                        self.broadcast_string( newstr, sock )
 
-while 1:
-    data = conn.recv(BUFFER_SIZE) or 'con_close'
-    if data == 'con_close':
-        conn.send(data)
-        break
-    print "received data:", data
-    conn.send(str(data) + '--' + '(This is a server reply)')  # echo
+    def accept_new_connection( self ):
+        newsock, (remhost, remport) = self.srvsock.accept()
+        self.descriptors.append( newsock )
 
-conn.close()
+        newsock.send("You're connected to the Python server\r\n")
+        str = 'Client joined %s:%s\r\n' % (remhost, remport)
+        self.broadcast_string( str, newsock )
+
+    def broadcast_string( self, str, omit_sock ):
+        for sock in self.descriptors:
+            if sock != self.srvsock and sock != omit_sock:
+                sock.send(str)
+        print str,
+
+myServer = ChatServer( 2626 ).run()
