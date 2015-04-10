1441251900fd797cb7dbdad73e62845c6fbda49a
Srinivas Garlapati <srinivasa.b.garlapati@gmail.com>
2015-04-10 23:05:58 +0530

    More in sockets

--- server.py
+++ server.py
@@ -1,65 +1,53 @@
-#!/usr/bin/env python
-
 import socket
-import select
+import threading
 
 
-class ChatServer:
-
-    max_clients = 3
-
-    def __init__(self, port):
-        self.port = port
-
-        self.srvsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.srvsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.srvsock.bind(("", port))
-        self.srvsock.listen(5)
-
-        self.descriptors = [self.srvsock]
-
-        print 'Server started on port %s' % port
+host = ''
+port = 2626
+connectionSevered = 0
 
 
+class client(threading.Thread):
+
+    def __init__(self, conn):
+        super(client, self).__init__()
+        self.conn = conn
+        self.data = ""
 
     def run(self):
-        while 1:
-            # Await an event on a readable socket descriptor
-            (sread, swrite, sexc) = select.select(self.descriptors, [], [])
-            # Iterate through the tagged read descriptors
-            for sock in sread:
-                # Received a connect to the server (listening) socket
-                if sock == self.srvsock:
-                    self.accept_new_connection()
-                else:
-                    # Received something on a client socket
-                    str = sock.recv(100)
+        while True:
+            self.data = self.data + self.conn.recv(1024)
+            if self.data.endswith(u"\n"):
+                print self.data
+                self.data = ""
 
-                    # Check to see if the peer socket closed
-                    if str == '':
-                        host, port = sock.getpeername()
-                        str = 'Client left %s:%s\r\n' % (host, port)
-                        self.broadcast_string(str, sock)
-                        sock.close
-                        self.descriptors.remove(sock)
-                    else:
-                        host, port = sock.getpeername()
-                        newstr = '[%s:%s] %s' % (host, port, str)
-                        self.broadcast_string(newstr, sock)
+    def send_msg(self, msg):
+        self.conn.send(msg)
 
-    def accept_new_connection(self):
-        newsock, (remhost, remport) = self.srvsock.accept()
-        self.descriptors.append(newsock)
+    def close(self):
+        self.conn.close()
 
-        print newsock.__hash__()
-        newsock.send("You're connected to the Python server\r\n")
-        str = 'Client joined %s:%s\r\n' % (remhost, remport)
-        self.broadcast_string(str, newsock)
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(5)
+except socket.error:
+    import sys
+    print 'Failed to create socket'
+    sys.exit()
 
-    def broadcast_string(self, str, omit_sock):
-        for sock in self.descriptors:
-            if sock != self.srvsock and sock != omit_sock:
-                sock.send(str)
-        print str,
+print '[+] Listening for connections on port: {0}'.format(port)
 
-myServer = ChatServer(2626).run()
+
+conn, address = s.accept()
+c = client(conn)
+c.start()
+print '[+] Client connected: {0}'.format(address[0])
+c.send_msg(u"How are you?")
+print "connectionSevered:{0}".format(connectionSevered)
+while (connectionSevered == 0):
+    try:
+        response = raw_input()
+        c.send_msg(response + u"\n")
+    except:
+        c.close()
