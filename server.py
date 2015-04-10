3ccb02dcf597574ccdbab34e7c89563c89ee33ee
Srinivas Garlapati <srinivasa.b.garlapati@gmail.com>
2015-04-10 11:22:41 +0530

    Enhancing more in server side..

--- server.py
+++ server.py
@@ -3,23 +3,29 @@
 import socket
 import select
 
+
 class ChatServer:
 
-    def __init__( self, port ):
-        self.port = port;
+    max_clients = 3
 
-        self.srvsock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-        self.srvsock.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
-        self.srvsock.bind( ("", port) )
-        self.srvsock.listen( 5 )
+    def __init__(self, port):
+        self.port = port
+
+        self.srvsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.srvsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.srvsock.bind(("", port))
+        self.srvsock.listen(5)
 
         self.descriptors = [self.srvsock]
+
         print 'Server started on port %s' % port
 
-    def run( self ):
+
+
+    def run(self):
         while 1:
             # Await an event on a readable socket descriptor
-            (sread, swrite, sexc) = select.select( self.descriptors, [], [] )
+            (sread, swrite, sexc) = select.select(self.descriptors, [], [])
             # Iterate through the tagged read descriptors
             for sock in sread:
                 # Received a connect to the server (listening) socket
@@ -31,28 +37,29 @@ class ChatServer:
 
                     # Check to see if the peer socket closed
                     if str == '':
-                        host,port = sock.getpeername()
+                        host, port = sock.getpeername()
                         str = 'Client left %s:%s\r\n' % (host, port)
-                        self.broadcast_string( str, sock )
+                        self.broadcast_string(str, sock)
                         sock.close
                         self.descriptors.remove(sock)
                     else:
-                        host,port = sock.getpeername()
+                        host, port = sock.getpeername()
                         newstr = '[%s:%s] %s' % (host, port, str)
-                        self.broadcast_string( newstr, sock )
+                        self.broadcast_string(newstr, sock)
 
-    def accept_new_connection( self ):
+    def accept_new_connection(self):
         newsock, (remhost, remport) = self.srvsock.accept()
-        self.descriptors.append( newsock )
+        self.descriptors.append(newsock)
 
+        print newsock.__hash__()
         newsock.send("You're connected to the Python server\r\n")
         str = 'Client joined %s:%s\r\n' % (remhost, remport)
-        self.broadcast_string( str, newsock )
+        self.broadcast_string(str, newsock)
 
-    def broadcast_string( self, str, omit_sock ):
+    def broadcast_string(self, str, omit_sock):
         for sock in self.descriptors:
             if sock != self.srvsock and sock != omit_sock:
                 sock.send(str)
         print str,
 
-myServer = ChatServer( 2626 ).run()
+myServer = ChatServer(2626).run()
