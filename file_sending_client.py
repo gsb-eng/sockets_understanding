6ff2a6365a58fecae45c52926653973b6f7982e3
Srinivas Garlapati <srinivasa.b.garlapati@gmail.com>
2015-04-12 23:45:32 +0530

    Added more to file sending client server

--- file_sending_client.py
+++ file_sending_client.py
@@ -8,10 +8,11 @@ serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverIP,serverPort))
 path = raw_input('Input Path:')
-print path
+
 
 if os.path.exists(path):
     clientSocket.send(bytes(path))
+    path_resp = clientSocket.recv(1024)
 
 
     if os.path.isfile(path):
@@ -24,18 +25,38 @@ if os.path.exists(path):
 
 
     else:
+        file_list = []
         for root, dirnames, filenames in os.walk(path):
             for filename in fnmatch.filter(filenames, '*.*'):
-                server_response = clientSocket.recv(1024)
-                if server_response == "clear":
-                    print( os.path.join(root, filename).encode('utf-8') )
-                    clientSocket.send(os.path.join(root, filename).encode('utf-8'))
-                    file = open(root+"/"+filename, "rb")
-                    data = file.read(1024)
-                    while (data):
-                        clientSocket.send(data)
-                        data = file.read(1024)
-                    file.close()
+                file_list.append(os.path.join(root, filename).encode('utf-8'))
+        print file_list
+        file_list = iter(file_list)
+        while True:
+            file_transfer_done = False
+            try:
+                file_name = file_list.next()
+            except StopIteration, e:
+                str(e)
+                break
+
+            print('Sent file name to server %s' % file_name)
+            clientSocket.send(file_name)
+            file_resp = clientSocket.recv(1024)
+            print('Received server response : %s' % file_resp)
+            file = open(file_name, "rb")
+            data = file.read(1024)
+            while True:
+                if data:
+                    clientSocket.send(data)
+                    print('Sent data to server')
+                    data_resp = clientSocket.recv(1024)
+                    print('Received server responce : %s' % data_resp)
+                data = file.read(1024)
+                if not data:
+                    clientSocket.send('file_closed')
+                    close_resp = clientSocket.recv(1024)
+                    break
+            file.close()
         clientSocket.send("close".encode('utf-8'))
 
 
--- file_sending_server.py
+++ file_sending_server.py
@@ -10,27 +10,7 @@ print('The server is ready to receive')
 connectionSocket, address = serverSocket.accept()
 path = connectionSocket.recv(1024)
 
-print path + ' server pathhh received'
-
-def write_file(path=None):
-    if not path:
-        path = connectionSocket.recv(1024)
-    print(path) + '  --Here'
-    if path.decode('utf-8')=="":
-        raise TypeError('Should be of utf-8')
-    fileName = os.path.basename(path).decode('utf-8')
-    print fileName + '  At file decode'
-    drive, tail = os.path.splitdrive( os.path.dirname(path) )
-    directory = os.getcwd()+tail.decode('utf-8')
-    if not os.path.exists(directory):
-	os.makedirs(directory)
-
-    file = open( directory +"/"+ fileName, 'wb' ) #open in binary
-    data = connectionSocket.recv(1024)
-    print data
-    file.write(data)
-    return data
-
+connectionSocket.send('path_received')
 
 if os.path.isfile(path):
     file = open(os.path.basename(path), 'wb' ) #open in binary
@@ -43,19 +23,35 @@ if os.path.isfile(path):
 
 else:
     while True:
-        try:
-            data = write_file()
-            while (data):
-                data = connectionSocket.recv(1024)
-                write_file(data)
-            file.close()
-        except TypeError, e:
-            print str(e)
+        path = connectionSocket.recv(1024)
+        print('Received file from client: %s' % path)
+        connectionSocket.send('file_path_received')
+        print('Sent responce to client : %s' % ('file_path_received'))
+        print(path)
+        if path.decode('utf-8')=="":
             break
+        fileName = os.path.basename(path)
+        print('File name received : %s' % fileName)
+        drive, tail = os.path.splitdrive( os.path.dirname(path) )
+        directory = os.getcwd()+tail
+        print directory
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        file = open( directory +"/"+ fileName, 'wb' ) #open in binary
+        print('New file created : %s' % fileName)
+        data = connectionSocket.recv(1024)
+
+        while True:
+            file.write(data)
+            print('File write done')
+            connectionSocket.send('Processed')
+            data = connectionSocket.recv(1024)
+            if 'file_closed' in data:
+                connectionSocket.send('file_done')
+                break
+        file.close()
 
 
 connectionSocket.close()
 print("connection closed")
-
-
-
